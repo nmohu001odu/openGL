7f74144ee867216c16eff0df86fc2fb05c936b9d
Natalie Mohun <nataliemohun@macbookpro.attlocal.net>
2024-11-20 20:53:22 -0800

    opengl

---
+++ open_gl/lib/common/shader.hpp
@@ -0,0 +1,6 @@
+#ifndef SHADER_HPP
+#define SHADER_HPP
+#include "/Users/nataliemohun/open_gl/lib/lib/GL/GL/glew.h"
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
+#endif
